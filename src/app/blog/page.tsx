5a084cdb923a23cfc1a91c2dcb70bd1b10cd7316
davincios <jvf.hus@gmail.com>
2025-05-19 01:53:58 -0400

    blog tuning

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 export default function TechnologyPage() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-full min-h-screen pt-20">
       Blog page comes here
     </main>
   );
