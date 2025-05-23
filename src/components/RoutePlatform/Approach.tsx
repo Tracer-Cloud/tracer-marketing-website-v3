188a1829d99d2ad3f40f97b54c074dfbd5f02cdd
Cpgreenig <96381060+Cpgreenig@users.noreply.github.com>
2025-05-23 13:31:57 +0100

    Align gridlines with card centers and expand card width
    - Center gridlines within each grid column for card overlap effect
    - Expand container max-width to 1800px for wider cards
    - Reduce side padding to px-2 for more horizontal space
    - Maintain card gaps and responsive design
    - Cards now appear wider and shorter with gridlines running through centers

--- src/components/RoutePlatform/Approach.tsx
+++ src/components/RoutePlatform/Approach.tsx
@@ -4,17 +4,23 @@ import React from 'react'
 const Approach = () => {
     return (
         <div className='bg-white pt-10 pb-4 md:py-[72px] w-full text-black border-t border-[#E8E8E8] flex items-center relative justify-center flex-col'>
-            <div className='w-full h-full max-w-[1140px] grid grid-cols-3 absolute top-0 900:px-0 px-4'>
-                <div className='border-l border-[#E8E8E8]' />
-                <div className='border-x-[1px] border-[#E8E8E8]' />
-                <div className='border-r border-[#E8E8E8]' />
+            <div className='w-full h-full max-w-[1800px] grid grid-cols-3 absolute top-0 900:px-0 px-2'>
+                <div className='flex justify-center'>
+                    <div className='border-l border-[#E8E8E8] h-full' />
+                </div>
+                <div className='flex justify-center'>
+                    <div className='border-l border-[#E8E8E8] h-full' />
+                </div>
+                <div className='flex justify-center'>
+                    <div className='border-l border-[#E8E8E8] h-full' />
+                </div>
             </div>
-            <div className='w-full flex z-[10]  flex-col items-center justify-center max-w-[1449px] px-4'>
+            <div className='w-full flex z-[10]  flex-col items-center justify-center max-w-[1800px] 900:px-0 px-2'>
                 <div className='w-full  pb-8 md:pb-12 font-[400] font-britti-sans text-[32px] md:text-[40px] text-[#202020] leading-[0.9]'>
                     The dawn of AI <br className='sm:hidden ' /> in science <br className='sm:flex hidden' /> requires <br className='sm:hidden ' />a new approach
                 </div>
-                <div className='grid 900:grid-cols-3 bg-white min-h-[251px] w-full gap-4 md:gap-6'>
-                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-8 md:gap-[46px] items-start justify-between'>
+                <div className='grid 900:grid-cols-3 bg-white min-h-[120px] w-full gap-4 md:gap-6'>
+                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-4 md:gap-[20px] items-start justify-between'>
                         <Image src={"/platform/icons/setting-icon.svg"} alt='setting-icon' width={65} height={65} className='shrink-0 aspect-square w-full max-w-[40px] sm:max-w-[56px]' />
                         <div className=''>
                             <h3 className='text-c-black font-[400] font-britti-sans text-[24px] md:text-[40px] leading-[1.1]'>
@@ -25,7 +31,7 @@ const Approach = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-8 md:gap-[46px] items-start justify-between'>
+                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-4 md:gap-[20px] items-start justify-between'>
                         <Image src={"/platform/icons/currency-dollar.svg"} alt='currency-dollar' width={65} height={65} className='shrink-0 aspect-square w-full max-w-[40px] sm:max-w-[56px]' />
                         <div className=''>
                             <h3 className='text-c-black font-[400] text-[24px] font-britti-sans md:text-[40px] leading-[1.1]'>
@@ -36,7 +42,7 @@ const Approach = () => {
                             </p>
                         </div>
                     </div>
-                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-8 md:gap-[46px] items-start justify-between'>
+                    <div className='border border-[#E8E8E8] p-4 flex flex-col gap-4 md:gap-[20px] items-start justify-between'>
                         <Image src={"/platform/icons/arrows-pointing-out.svg"} alt='arrows-pointing-out' width={65} height={65} className='shrink-0 aspect-square w-full max-w-[40px] sm:max-w-[56px]' />
                         <div className=''>
                             <h3 className='text-c-black font-[400] font-britti-sans text-[24px] md:text-[40px] leading-[1.1]'>
